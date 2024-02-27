90fc4849cc1acb92eea7cde65bd89a72adfae81a
Giacomo Pedretti <giacomo.pedretti@hpe.com>
2024-02-27 10:40:00 -0800

    housekeeping and copyright

--- countrycrab/compiler.py
+++ countrycrab/compiler.py
@@ -1,3 +1,20 @@
+###
+# Copyright (2024) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
+
 import numpy as np
 from pysat.solvers import Minisat22
 from pysat.formula import CNF
@@ -92,4 +109,5 @@ def compile_walksat_m(config: t.Dict, params: t.Dict) -> t.Union[t.Dict, t.Tuple
     tcam_cores = tcam.reshape((n_cores, -1, variables))
     ram_cores = ram.reshape((n_cores, -1, variables))
 
+    architecture = [tcam, ram, tcam_cores, ram_cores, n_cores]
     return tcam, ram, tcam_cores, ram_cores, n_cores
--- countrycrab/heuristics.py
+++ countrycrab/heuristics.py
@@ -1,3 +1,19 @@
+###
+# Copyright (2024) Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###
+
 import numpy as np
 import pandas as pd
 import os
@@ -12,6 +28,8 @@ def walksat_m(data):
     inputs = data.get('inputs')   
     tcam = data.get('tcam')
     ram = data.get('ram')
+    tcam_cores = data.get('tcam_cores')
+    ram_cores = data.get('ram_cores')
     violated_constr_mat = data.get('violated_constr_mat')
     n_cores = data.get('n_cores')
 
