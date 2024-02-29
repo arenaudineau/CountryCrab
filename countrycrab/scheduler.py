ee0911cd8a213fc83f0c133695c2230916b45bb4
Giacomo Pedretti <giacomo.pedretti@hpe.com>
2024-02-29 13:38:48 -0800

    towards merging scheduler and hpo

--- countrycrab/scheduler.py
+++ countrycrab/scheduler.py
@@ -11,6 +11,7 @@ from ray.air import RunConfig
 from solver import solve
 import argparse
 import json
+import numpy as np
 
 
 def schedule(config_fname: t.Optional[str] = None) -> None:   
@@ -18,10 +19,48 @@ def schedule(config_fname: t.Optional[str] = None) -> None:
     with open(config_path, 'r') as f:  
         config = json.load(f)
     instance_list = config["instance_list"]
+    if 'min_noise' in config:
+        min_noise = config["min_noise"]
+        max_noise = config["max_noise"]
+        num_samples = config['num_samples']
+    else:
+        min_noise = config['noise']
+        max_noise = config['noise']
+        num_samples = 1
+
+    # load parameters to save them in the configuration    
+    # p_solve is the probability of solving the problem
+    p_solve = config.get("p_solve", 0.99)
+    # task is the type of task to be performed
+    task = config.get("task", "debug")
+    # max runs is the number of parallel initialization (different inputs)
+    max_runs = config.get("max_runs", 100)
+    # max_flips is the maximum number of iterations
+    max_flips = config.get("max_flips", 1000)
+    # noise profile
+    noise_dist = config.get("noise_distribution",'normal')
+    # architecture specifics
+    n_words = config.get("n_words", -1) # -1 means that the number of words is equal to the number of clauses
+    n_cores = config.get("n_cores", 1)
+    scheduling = config.get("scheduling", "fill_first")
+    # heuristics specification
+    heuristic_name = config.get("heuristic", 'walksat_m')
+    compiler_name = config.get("compiler", 'compile_walksat_m')
 
     # the only search space parameters are the instances in the case of the scheduler
     search_space = {
+        "noise": tune.grid_search(np.linspace(min_noise, max_noise, num_samples).tolist()),
         "instance": tune.grid_search(instance_list),
+        "p_solve": p_solve,
+        "task": task,
+        "max_runs": max_runs,
+        "max_flips": max_flips,
+        "heuristic": heuristic_name,
+        "compiler": compiler_name,
+        "noise_distribution": noise_dist,
+        "n_cores": n_cores,
+        "n_words": n_words,
+        "scheduling": scheduling
     }
 
     # set resource per trial based on experiments. A high value is used to ensure that the experiment runs but it can be decreased to optimize the use of the GPU
