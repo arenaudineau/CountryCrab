44fb9d2ad9f0097743ab42a17621d94c22559ebf
Tinish Bhattacharya <bhattact@xtime-2.labs.hpecorp.net>
2024-03-22 12:22:20 -0700

    resolving compiler.py

--- countrycrab/compiler.py
+++ countrycrab/compiler.py
@@ -21,6 +21,7 @@ from pysat.formula import CNF
 import typing as t
 import math
 import cupy as cp
+import os
 
 def load_clauses_from_cnf(file_path: str) -> t.List[t.List[int]]:
     with open(file_path, 'r') as file:
@@ -132,23 +133,14 @@ def compile_walksat_m(config: t.Dict, params: t.Dict) -> t.Union[t.Dict, t.Tuple
 
 def compile_walksat_g(config: t.Dict, params: t.Dict) -> t.Union[t.Dict, t.Tuple]:
     instance_name = config["instance"]
-    # print("Compiling instance with walksat_g...")
-    # simple mapping, takes the instance and map it to a 'large' tcam and ram
-    # load instance
-    #formula = CNF(from_file=instance_name)
-    # extract clauses
-    #clauses = list(filter(None, formula.clauses))
-
-    instance_name = config["instance"]
+    vpr_netlist_loc = config["netlist_dir"]
     clauses_list = load_clauses_from_cnf(instance_name)
-    #print(clauses)
-    #for clause in clauses:
-    #    solver.add_clause(clause)
-    #clauses = np.array(list(filter(None, formula.clauses)))
-    #clauses = np.array(clauses)
 
     clauses = len(clauses_list)
     variables = count_variables(clauses_list)
+
+    num_wta_inputs = config.get("num_wta_inputs",variables)
+    scheduling = params.get("scheduling", "fill_first")
 
     # map clauses to TCAM
     ramf_array = np.zeros([2*variables,clauses])
@@ -162,8 +154,110 @@ def compile_walksat_g(config: t.Dict, params: t.Dict) -> t.Union[t.Dict, t.Tuple
             else:
                 ramf_array[2*(-literal-1)+1,i]=1
                 ramb_array[i,2*(-literal-1)+1]=1    
-    architecture = [ramf_array, ramb_array]
+    
+    params['variables'] = variables
+    params['clauses'] = clauses
+
+    if scheduling == "fill_first":
+        var_list = np.arange(variables)
+        num_superTiles = math.ceil(variables/num_wta_inputs)
+        superTile_varIndices = []
+        last_index = 0
+        for i in range(num_superTiles):
+            if variables%num_wta_inputs==0:
+                superTile_varIndices.append(list(var_list[last_index:last_index+num_wta_inputs]))
+                last_index = last_index+num_wta_inputs
+            else:
+                if i<num_superTiles-1:
+                    superTile_varIndices.append(list(var_list[last_index:last_index+num_wta_inputs]))
+                    last_index = last_index+num_wta_inputs
+                else:
+                    superTile_varIndices.append(list(var_list[last_index:]))
+                    last_var = var_list[variables-1]
+                    superTile_varIndices[i].extend([last_var]*(num_wta_inputs-int(variables%num_wta_inputs)))
+    elif scheduling == "vpr":
+        net_filename = vpr_netlist_loc+os.path.basename(instance_name).split(".cnf")[0]+'.net'
+        superTile_varIndices, num_superTiles = read_netlist(net_filename,variables,clauses,num_wta_inputs)
+        for i in range(num_superTiles):
+            if len(superTile_varIndices[i])<num_wta_inputs:
+                diff = num_wta_inputs - len(superTile_varIndices[i])
+                superTile_varIndices[i].extend([superTile_varIndices[i][-1]]*diff)
+                
+    architecture = [ramf_array, ramb_array, superTile_varIndices, num_superTiles]
     return architecture, params
+
+def read_netlist(net_filename,nvar,nclause,num_wta_inputs):
+    variables = nvar
+    file = open(net_filename, 'r')
+    lines = file.readlines()
+     
+    count = 0
+    vpr_tile_inputs = []
+    vpr_tile_outputs = []
+    for i,line in enumerate(lines):
+        if 'clb[' in line:
+            tile_inputs = lines[i+2]
+            tile_inputs = [int(j) for j in tile_inputs.split('>')[1].split('<')[0].replace("open","").replace("_x","").split()]
+            tile_outputs_temp = lines[i+5]
+            tile_outputs_temp = tile_outputs_temp.split('>')[1].split('<')[0].replace("open","").replace(" ","").split('-&gt;clbouts1')
+            num_outputs = sum([1 for _ in tile_outputs_temp])-1
+            curr_index = i+10
+            output_lines_read = 0
+            tile_outputs = []
+            while output_lines_read < num_outputs:
+                if "mode=\"default\"" in lines[curr_index] and "instance=\"ble[" in lines[curr_index]:
+                    tile_outputs.append(int(lines[curr_index].split()[1].split("\"")[1].split("_l")[1]))
+                    output_lines_read = output_lines_read + 1
+               
+                curr_index = curr_index + 1
+               
+            vpr_tile_inputs.append(tile_inputs)
+            vpr_tile_outputs.append(tile_outputs)
+           
+    
+    num_clbs = len(vpr_tile_outputs)
+    vpr_var_list = []
+    for i in range(num_clbs):
+        output_temp = np.array(vpr_tile_outputs[i])
+        vpr_var_list.append(list(output_temp[np.where(output_temp<nvar+1)[0]]-1))
+    
+    counter = 0
+    for i in range(num_clbs):
+        counter = counter + len(vpr_var_list[i])
+    
+    if counter!=nvar:
+        print("Variables Missing in Post-Packing Netlist!!!")
+       
+    num_superTiles = math.ceil(variables/num_wta_inputs)
+    superTile_varIndices = []
+    vpr_var_list_copy = vpr_var_list.copy()
+    for i in range(num_superTiles):
+        pointer = 0
+        superTile_varIndices.append(vpr_var_list_copy[pointer])
+        vpr_var_list_copy.remove(vpr_var_list_copy[pointer])
+        curr_len = len(superTile_varIndices[i])
+        if curr_len < num_wta_inputs:
+            done_flag = 0
+            pointer = 0
+            while done_flag==0 and len(vpr_var_list_copy)!=0:
+                if curr_len +len(vpr_var_list_copy[pointer]) <= num_wta_inputs:
+                    superTile_varIndices[i].extend(vpr_var_list_copy[pointer])
+                    vpr_var_list_copy.remove(vpr_var_list_copy[pointer])
+                    curr_len = len(superTile_varIndices[i])
+                    if len(vpr_var_list_copy)==0:
+                        break
+                    if pointer>=len(vpr_var_list_copy)-1 or len(vpr_var_list_copy)==0 or curr_len==num_wta_inputs:
+                        done_flag = 1
+                else:
+                    pointer = pointer + 1
+                    if pointer==len(vpr_var_list_copy) or curr_len==num_wta_inputs:
+                        done_flag = 1
+                   
+                       
+        if len(vpr_var_list_copy)==0:
+            break
+    
+    return superTile_varIndices, num_superTiles
 
 
 # QUBO mappings available
